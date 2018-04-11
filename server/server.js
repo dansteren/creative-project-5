058ce308d42ef0a4f7203f464c478b05cd63b18d
Manuel Marquez <meny_marquez@hotmail.com>
2018-04-10 21:13:55 -0600

    deleted the previous code endpoints from the server file

--- server/server.js
+++ server/server.js
@@ -28,48 +28,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// TODO: remove mock data on server.
-// const cards = {};
-const cards = require('./mockData');
 
-app.get('/api/cards', (req, res) => {
-  res.send(Object.values(cards));
-});
-
-app.get('/api/cards/:id', (req, res) => {
-  const card = cards[req.params.id];
-  if (!card) {
-    return res.status(404).send('Card does not exist.');
-  }
-  res.send(card);
-});
-
-app.post('/api/cards', ({ body: card }, res) => {
-  const id = uuid();
-  const newItem = { id, ...card };
-  cards[id] = newItem;
-  res.send(newItem);
-});
-
-app.put('/api/cards/:id', (req, res) => {
-  const id = req.params.id;
-  const card = cards[id];
-  if (!card) {
-    return res.status(404).send('Card does not exist.');
-  }
-  const newCard = { id, ...req.body };
-  cards[id] = newCard;
-  res.send(newCard);
-});
-
-app.delete('/api/cards/:id', (req, res) => {
-  const id = req.params.id;
-  const card = cards[id];
-  if (!card) {
-    return res.status(404).send('Card does not exist.');
-  }
-  delete cards[id];
-  res.sendStatus(200);
-});
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
